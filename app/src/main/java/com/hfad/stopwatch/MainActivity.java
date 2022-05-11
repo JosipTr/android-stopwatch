5c1208644e9318efe8d71c2fb41e414280893c3e
JosipTr <josiptroha@gmail.com>
2022-05-11 14:56:51 +0200

    Add handler to timer function

--- app/src/main/java/com/hfad/stopwatch/MainActivity.java
+++ app/src/main/java/com/hfad/stopwatch/MainActivity.java
@@ -3,6 +3,7 @@ package com.hfad.stopwatch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.TextView;
 
@@ -34,15 +35,23 @@ public class MainActivity extends AppCompatActivity {
 
     private void runTimer() {
         final TextView timeView = (TextView) findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds / 3600;
+                int minutes = (seconds % 3600) / 60;
+                int secs = seconds % 60;
+                String time = String.format(Locale.getDefault(), "%d:%02d:%02d", hours, minutes, secs);
+                timeView.setText(time);
+                if (running) {
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
 
-        int hours = seconds / 3600;
-        int minutes = (seconds % 3600) / 60;
-        int secs = seconds % 60;
-        String time = String.format(Locale.getDefault(), "%d:%02d:%02d", hours, minutes, secs);
-        timeView.setText(time);
-        if (running) {
-            seconds++;
-        }
+
     }
 
 }
